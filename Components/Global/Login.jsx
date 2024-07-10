02d45e0b5bab272943d829b251fa595613800e96
alok2424 <22ec059@jssaten.ac.in>
2024-07-10 11:55:42 +0530

    commit

--- Components/Global/Login.jsx
+++ Components/Global/Login.jsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react'
 
-const Login = () => {
+const Login = ({setActiveComponent, axios,notifyError,notifySuccesss }) => {
+  const [user, setUser] = useState({
+    password: "",
+    passwordConfirm: "",
+  });
+  
+  const handleFormFileChange = (fieldName, e) => {
+    setUser({ ...user, [fieldName]: e.target.value });
+  };
+
+  const apiLogin = async (e) =>{
+    e.preventDefault();
+    if(
+      user.email == "" ||
+      user.password == "" 
+    ){
+      return notifyError("Please provide all the details");
+    }
+    notifySuccesss("wait creating account...");
+  }
   return (
-    <div>Login</div>
+    <div className="techwave_fn_sign">
+    <div className="sign__content">
+      <h1 className="logo">Designed by alok</h1>
+      <form className="login">
+        <div className="form__content">
+          <div className="form__title">SignIn</div>
+
+          <div className="form__username">
+            <label htmlFor="user_login">Email</label>
+            <input
+              type="text"
+              className="input"
+              onChange={(e) => handleFormFileChange("Email", e)}
+            ></input>
+          </div>
+
+          <div className="form__username">
+            <label htmlFor="user_login">Password</label>
+            <input
+              type="text"
+              className="input"
+              onChange={(e) => handleFormFileChange("password", e)}
+            ></input>
+          </div>
+
+          
+          <div className="form__alternative">
+          <a onClick={(e)=>apiLogin(e)}
+          className="techwave_fn_button">
+          <span>Login</span>
+          </a>
+          </div>
+
+          <div></div>
+        </div>
+      </form>
+
+      <div className="sign__desc">
+      <p>
+      Not a member?
+      <a onClick={()=>setActiveComponent("SignUp")}>
+      Login
+      </a>
+      </p>
+      </div>
+    </div>
+  </div>
   )
 }
 
--- Components/Global/SignUp.jsx
+++ Components/Global/SignUp.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+const SignUp = ({ axios, setActiveComponent ,notifyError,notifySuccesss}) => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
 
-export default SignUp
+  const handleFormFileChange = (fieldName, e) => {
+    setUser({ ...user, [fieldName]: e.target.value });
+  };
+
+  const createAccount = async (e) => {
+    e.preventDefault();
+    if (
+      user.name == "" ||
+      user.email == "" ||
+      user.password == "" ||
+      user.passwordConfirm == ""
+    ) {
+      return notifyError("Please provide all the details");
+    }
+    notifySuccesss("wait creating account...");
+
+    try {
+      //Api call
+    } catch (error) {}
+  };
+    return (
+      <div className="techwave_fn_sign">
+        <div className="sign__content">
+          <h1 className="logo">Designed by alok</h1>
+          <form className="login">
+            <div className="form__content">
+              <div className="form__title">SignUp</div>
+              <div className="form__username">
+                <label htmlFor="user_login">Name</label>
+                <input
+                  type="text"
+                  className="input"
+                  onChange={(e) => handleFormFileChange("name", e)}
+                ></input>
+              </div>
+
+              <div className="form__username">
+                <label htmlFor="user_login">Email</label>
+                <input
+                  type="text"
+                  className="input"
+                  onChange={(e) => handleFormFileChange("Email", e)}
+                ></input>
+              </div>
+
+              <div className="form__username">
+                <label htmlFor="user_login">Password</label>
+                <input
+                  type="text"
+                  className="input"
+                  onChange={(e) => handleFormFileChange("password", e)}
+                ></input>
+              </div>
+
+              <div className="form__username">
+                <label htmlFor="user_login">ConfirmPassword</label>
+                <input
+                  type="text"
+                  className="input"
+                  onChange={(e) => handleFormFileChange("passwordConfirm", e)}
+                ></input>
+              </div>
+   
+              <div className="form__alternative">
+              <a onClick={(e)=>createAccount(e)}
+              className="techwave_fn_button"
+              >
+              <span>Create Account</span>
+              </a>
+              </div>
+
+              <div></div>
+            </div>
+          </form>
+
+          <div className="sign__desc">
+          <p>
+          Not a member?
+          <a onClick={()=>setActiveComponent("Login")}>
+          Login
+          </a>
+          </p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+export default SignUp;
